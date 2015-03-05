5ae750275cb9df2d0af3f1e7205f50b77968aaf7
jolth <jolthgs@gmail.com>
2015-03-05 12:06:52 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -10,21 +10,27 @@
 
 
 PWD=$(pwd)
+vimrc=~/.vimrc
 
 #       Configure 
 #fast saving:
 #echo "bind -r '\C-s' ; stty -ixon" >> ~/.bashrc;source ~/.bashrc
 echo -e "bind -r '\C-s'\nstty -ixon" >> ~/.bashrc;source ~/.bashrc
 
+# Check that Git exists 
 if [ ! -e $(which git) ]; then
     echo "Git is required, install please git"
     exit 1
 fi
 
-if [ -e ~/.vimrc ]; then
+#if [ -e ~/.vimrc ]; then
+if [ -e $vimrc ]; then
+    #if [ -L ~/.vimrc ]; then
+    #    rm 
+    #fi
     echo ".vimrc file exists"
     echo "backup into raw created"
-    cp ~/.vimrc_{,$(date +'%d%m%y%H%M%S')}
+    mv ~/.vimrc{,_$(date +'%d%m%y%H%M%S')}
 fi
 
 if [ -d ~/.vim ]; then
