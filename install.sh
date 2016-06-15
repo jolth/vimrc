2ace27ecfa6dfe16fc9324fe24351e386ec881a2
Jolth <jolthgs@gmail.com>
2016-06-15 07:47:58 -0500

    update fast access to vimrc

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-
 #
 # Script to install my vimrc
 #
 # curl -sL https://raw.githubusercontent.com/jolth/vimrc/master/install.sh | bash -
-#   or
+# or
 # wget -qO- https://raw.githubusercontent.com/jolth/vimrc/master/install.sh | bash -
 #
 
